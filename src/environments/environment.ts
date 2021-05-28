91cd1fde5969de2dd6b10cfa354bce2897c3e1ee
Myhailo Dmytrash <myhailo.dmytrash@gmail.com>
2021-05-28 12:08:11 +0300

    make it better

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiBaseUrl: 'https://for-it-in-net-server.herokuapp.com/',
   authenticationCookieName: 'X-Authentication-Token',
   csrf: 'XSRF-TOKEN',
